621773de41e333c52671f90b52dd71cc12ae5147
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-12-08 14:22:15 -0600

    Adjusted the line height and letter spacing of the message when no events matches the filter criteria!

--- src/components/Events/EventsDisplay.jsx
+++ src/components/Events/EventsDisplay.jsx
@@ -11,7 +11,16 @@ const EventsDisplay = ({ events }) => {
   console.log('Events received in EventsDisplay:', events);
 
   if (!events || events.length === 0) {
-    return <p style={{color: '#FFF8D6', fontFamily: "Antonio"}} className="flex flex-wrap text-3xl font-bold p-4 w-[90%] justify-center text-center">We couldn’t find any events matching for your search. Don’t worry — new events are added regularly, so check again soon!</p>   
+    return <p 
+        style={{
+          color: '#FFF8D6', 
+          lineHeight: "125%", 
+          letterSpacing: "0.02em"
+        }} 
+        className="flex flex-wrap text-3xl font-bold p-4 w-[90%] justify-center text-center baseText"
+      >
+        We couldn’t find any events matching for your search. Don’t worry — new events are added regularly, so check again soon!
+      </p>   
   }
 
   return (
